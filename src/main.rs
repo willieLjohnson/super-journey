f7318118ab0a04d448cdda69b1d578b95c2add9a
Willie Johnson <liwa.johnson@gmail.com>
2021-11-20 15:14:27 -0500

    Refactor code into seperate files

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,22 @@
 use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
 use specs::prelude::*;
+
+mod components;
+pub use components::*;
+mod map;
+pub use map::*;
+mod player;
+pub use player::*;
+
 use specs_derive::Component;
-use std::cmp::{max, min};
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
 
 #[derive(Component)]
 struct LeftMover {}
 
 struct LeftWalker {}
 
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(PartialEq, Copy, Clone)]
-enum TileType {
-    Wall,
-    Floor,
-}
-
-struct State {
+pub struct State {
     ecs: World,
-}
-
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
-}
-
-fn new_map() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80 * 50];
-
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, 49)] = TileType::Wall;
-    }
-
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(79, y)] = TileType::Wall;
-    }
-
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-        if idx != xy_idx(40, 25) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-fn draw_map(map: &[TileType], ctx: &mut Rltk) {
-    let mut y = 0;
-    let mut x = 0;
-
-    for tile in map.iter() {
-        match tile {
-            TileType::Floor => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.5, 0.5, 0.5),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('.'),
-                );
-            }
-            TileType::Wall => ctx.set(
-                x,
-                y,
-                RGB::from_f32(0.0, 1.0, 0.0),
-                RGB::from_f32(0., 0., 0.),
-                rltk::to_cp437('#'),
-            ),
-        }
-
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-
-        if map[destination_idx] != TileType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    match ctx.key {
-        None => {}
-        Some(key) => match key {
-            VirtualKeyCode::A => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::D => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::W => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::S => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
 }
 
 impl GameState for State {
---
+++ src/player.rs
@@ -0,0 +1,32 @@
+use super::{xy_idx, Player, Position, State, TileType};
+use rltk::{Rltk, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+
+pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+  let mut positions = ecs.write_storage::<Position>();
+  let mut players = ecs.write_storage::<Player>();
+  let map = ecs.fetch::<Vec<TileType>>();
+
+  for (_player, pos) in (&mut players, &mut positions).join() {
+    let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+    if map[destination_idx] != TileType::Wall {
+      pos.x = min(79, max(0, pos.x + delta_x));
+      pos.y = min(49, max(0, pos.y + delta_y));
+    }
+  }
+}
+
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+  match ctx.key {
+    None => {}
+    Some(key) => match key {
+      VirtualKeyCode::A => try_move_player(-1, 0, &mut gs.ecs),
+      VirtualKeyCode::D => try_move_player(1, 0, &mut gs.ecs),
+      VirtualKeyCode::W => try_move_player(0, -1, &mut gs.ecs),
+      VirtualKeyCode::S => try_move_player(0, 1, &mut gs.ecs),
+      _ => {}
+    },
+  }
+}
