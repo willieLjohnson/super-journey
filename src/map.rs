84988cfc0ec5037c7a29097812a08c6246f2001c
Willie Johnson <liwa.johnson@gmail.com>
2021-11-20 15:29:28 -0500

    Create function for creating rooms in map

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,4 @@
+use super::Rect;
 use rltk::{RandomNumberGenerator, Rltk, RGB};
 use std::cmp::{max, min};
 
@@ -43,7 +44,21 @@ pub fn new_map_test() -> Vec<TileType> {
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
   let mut map = vec![TileType::Wall; 80 * 50];
 
+  let room1 = Rect::new(20, 15, 10, 15);
+  let room2 = Rect::new(35, 15, 10, 15);
+
+  apply_room_to_map(&room1, &mut map);
+  apply_room_to_map(&room2, &mut map);
+
   map
+}
+
+fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
+  for y in room.y1 + 1..=room.y2 {
+    for x in room.x1 + 1..=room.x2 {
+      map[xy_idx(x, y)] = TileType::Floor;
+    }
+  }
 }
 
 pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
