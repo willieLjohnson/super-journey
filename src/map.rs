ec669d51774317eedc0dbede87a187e767381dfd
Willie Johnson <liwa.johnson@gmail.com>
2021-11-21 14:06:51 -0500

    Complete overhaul of gameplay mechanics

--- src/map.rs
+++ src/map.rs
@@ -61,6 +61,24 @@ fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
   }
 }
 
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+  for x in min(x1, x2)..=max(x1, x2) {
+    let idx = xy_idx(x, y);
+    if idx > 0 && idx < 80 * 50 {
+      map[idx as usize] = TileType::Floor;
+    }
+  }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+  for y in min(y1, y2)..=max(y1, y2) {
+    let idx = xy_idx(x, y);
+    if idx > 0 && idx < 80 * 50 {
+      map[idx as usize] = TileType::Floor;
+    }
+  }
+}
+
 pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
   let mut y = 0;
   let mut x = 0;
